db93d62157705aa5d3f73600aca9474de6b17936
Md. Abdul Kader <abdulemail789@gmail.com>
2024-01-16 10:51:32 +0600

    created test middleware

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { registerController, loginController, testController } = require('../controllers/auth.controller');
-
+const { requireSignIn, isAdmin } = require('../middleware/authMiddleware')
 router.route('/register').post(registerController)
 router.route('/login').post(loginController)
+router.route("/test").get(requireSignIn, isAdmin, testController);
 
 
 
