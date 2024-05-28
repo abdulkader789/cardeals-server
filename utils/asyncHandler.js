84f0b890048686d424af3d90fc19ea5016afe27b
Md. Abdul Kader <abdulemail789@gmail.com>
2024-05-28 06:05:25 +0600

    added admin middle ware

--- utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next))
-            .catch((err) => next(err)); // Pass any errors to the express error handler
-    };
-};
-
-module.exports = asyncHandler;
