9a37bd20571e7b884129f42dfc3f8fc2b64e6ea9
Md. Abdul Kader <abdulemail789@gmail.com>
2024-05-31 00:46:03 +0600

    added running msg

--- src/index.js
+++ src/index.js
@@ -23,7 +23,9 @@ import brandRouter from './routes/brand.routes.js'
 import modelRouter from './routes/model.routes.js'
 import carRouter from './routes/car.routes.js'
 
-
+app.get('/src', (req, res) => {
+    res.send("<h1>Node Server is Running Successfully</h1>")
+})
 
 
 //routes declaration
