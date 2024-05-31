8172c32954b01026d123f78c6169ca312793dbcd
Md. Abdul Kader <abdulemail789@gmail.com>
2024-05-31 08:25:57 +0600

    added client link to cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import connectDB from './db/connect.js';
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://abdulkader789-cardeals.netlify.app/",
     credentials: true
 }))
 
